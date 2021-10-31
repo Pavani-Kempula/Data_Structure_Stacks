b30b5c943e8b4aa59493774cef87c85b381d9fb6
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-01 03:02:05 +0530

    ADDED : Welcome to Data Structure Stacks Program

---
+++ src/main/java/com/bridgelabz/stack/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        //Displaying Welcome Message
+        System.out.println("Welcome to Data Structure Stacks Program");
+    }
+}
